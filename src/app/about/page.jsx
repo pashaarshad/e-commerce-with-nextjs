d24fedd04a470f70be70645b671c720b3dc506e8
pashaarshad <arshadpashaintern@gmail.com>
2025-08-08 17:37:27 +0530

    working the next pages

---
+++ src/app/about/page.jsx
@@ -0,0 +1,12 @@
+// import 'react' from 'react';
+import React from 'react';
+function About() {
+  return (
+    <main>
+      <h1>About Page</h1>
+      <p>This is the about page of our application.</p>
+    </main>
+  );
+}
+
+export default About;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,12 @@
-'use client';
+import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <>
-
       <main>
-        HOME PAGE
+        <h1> HOME PAGE</h1>
       </main>
-
     </>
   );
 }
-
-import "./globals.css";
