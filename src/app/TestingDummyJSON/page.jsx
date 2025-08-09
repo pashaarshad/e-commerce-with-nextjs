50e44df16e3ad0cd0a7b354c6b90571a3adb3c35
pashaarshad <arshadpashaintern@gmail.com>
2025-08-09 17:12:05 +0530

    ok

---
+++ src/app/TestingDummyJSON/page.jsx
@@ -0,0 +1,12 @@
+// import 'react' from 'react';
+import React from 'react';
+function TestingDummyJSON() {
+  return (
+    <main>
+      <h1>Testing Dummy JSON</h1>
+      <p>This is a page for testing dummy JSON data.</p>
+    </main>
+  );
+}
+
+export default TestingDummyJSON;
